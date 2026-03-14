89b811ca3b7ee3b2da33d2be4133c75fb22fa776
Austin <austindoq@gmail.com>
2026-03-14 15:47:32 -0700

    Continue logic for tailwindClassBuilder

--- tailwindClassBuilder.js
+++ tailwindClassBuilder.js
@@ -4,26 +4,57 @@ import tailwindClassList from "./tailwind-classes.json" with { type: "json" }; /
 // against the Tailwind category master list.
 
 const userClasses = ["flex-row", "pt-4", "mt-2", "mb-4", "pb-3", "flex", "h-4"];
+const tempUserClasses = [
+  "flex-row",
+  "pt-4",
+  "mt-2",
+  "mb-4",
+  "pb-3",
+  "flex",
+  "h-4",
+];
 
 function tailwindClassBuilder(userClasses, tailwindClassList) {
-  let categoriezedClasses = {};
+  let categorizedClasses = {};
 
   console.log(Object.keys(tailwindClassList));
 
   for (const userClass of userClasses) {
     console.log(userClass);
     for (const category in tailwindClassList) {
-      if (tailwindClassList[category].include(userClass)) {
+      if (tailwindClassList[category].includes(userClass)) {
         console.log(`The class ${userClass} is in the master list.`);
-      } else {
-        console.log(`The class ${userClass} is not a tailwind class.`);
+
+        if (category in categorizedClasses) {
+          categorizedClasses[category].push(userClass);
+          let classIndex = tempUserClasses.indexOf(userClass);
+          tempUserClasses.pop(classIndex);
+          // tempUserClasses.remove(userClass);
+        } else {
+          categorizedClasses[category] = [userClass];
+          let classIndex = tempUserClasses.indexOf(userClass);
+          tempUserClasses.pop(classIndex);
+        }
       }
     }
-  }
 
-  for (const category in tailwindClassList) {
-    console.log(tailwindClassList[category]);
+    // for (const category in tailwindClassList) {
+    //   if (tailwindClassList[category].includes(userClass)) {
+    //     console.log(`The class ${userClass} is in the master list.`);
+
+    //     if (category in categorizedClasses) {
+    //       categorizedClasses[category].push(userClass);
+    //     } else {
+    //       categorizedClasses[category] = [userClass];
+    //     }
+    //   }
+    // }
   }
+  console.log(tempUserClasses);
+
+  // for (const category in tailwindClassList) {
+  //   console.log(tailwindClassList[category]);
+  // }
 }
 
 tailwindClassBuilder(userClasses, tailwindClassList);
