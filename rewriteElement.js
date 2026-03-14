3e79788f6a6c0444e1dd0e0ce2cfe1518a573a4b
Luka Gajic <luka.gajic2013@gmail.com>
2026-03-14 16:54:13 -0700

    Add rewriteElement.js

---
+++ rewriteElement.js
@@ -0,0 +1,12 @@
+// placeholder
+let sortedArray = ['border-teal-700', 'bg-blue-50', 'flex', 'flex-row', 'hello', 'world']
+
+function rewriteElement(sortedArray){
+    let sortedString = ""
+    for(const item of sortedArray){
+        sortedString += " " + item
+    }
+    return sortedString
+}
+
+console.log(rewriteElement(sortedArray))
