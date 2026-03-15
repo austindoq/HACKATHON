df2839ed8f9fe0484d5a43062e9991a44333260c
Luka Gajic <luka.gajic2013@gmail.com>
2026-03-15 08:41:55 -0700

    update parser

--- fileParser.js
+++ fileParser.js
@@ -1,114 +1,66 @@
-// removed: import { createRequire } from "module";
 import { readData } from "./formSubmit.js";
 import { tailwindClassBuilder } from "./tailwindClassBuilder.js";
 import tailwindClassList from "./tailwind-classes.json" with { type: "json" };
 import { sortTailwindClasses } from "./sortTailwindClasses.js";
 import { rewriteElement } from "./rewriteElement.js";
 
-// removed: createRequire and JSSoup setup
-
-// removed: getFileContents() - not needed, readData() handles file input
-
-function writeNewFile(soup) {
-  try {
-    fs.writeFileSync("dummy-new.html", soup);
-  } catch (error) {
-    console.log(`${error}`);
-  }
-}
-
 function createSoup(fileContents) {
   const parser = new DOMParser();
-  // console.log(parser.parseFromString(fileContents, "text/html"));
   return parser.parseFromString(fileContents, "text/html");
 }
 
 function createClassList(classes) {
-  return classes.split(" ");
+  return classes.trim().split(/\s+/);
 }
 
 function iterateElements(soup) {
-  let currentElementClasses;
-  let categorizedClasses;
-  let sortedClassList;
-  let sortedClassString;
-  let currentElement = soup.body.firstElementChild; // replaced: soup.find("body")
-  // console.log(currentElement); //Element GOOD
+  const userOrderPref =
+    JSON.parse(localStorage.getItem("userCategoryOrder")) || [];
 
-  // MOCK
-  // let userOrderPref = [
-  //   "border color",
-  //   "colors",
-  //   "flex",
-  //   "paddings",
-  //   "text color",
-  // ];
+  const allElements = soup.body.querySelectorAll("*");
 
-  const userOrderPref = JSON.parse(localStorage.getItem("userCategoryOrder"));
+  allElements.forEach((element) => {
+    if (element.className && typeof element.className === "string") {
+      const currentElementClasses = createClassList(element.className);
 
-  while (currentElement !== null) {
-    if (currentElement.nodeType === Node.TEXT_NODE) {
-      // replaced: currentElement._text
-      currentElement = currentElement.nextElementSibling;
-
-      continue;
-    }
-    if (currentElement.className) {
-      // replaced: currentElement.attrs.class
-      currentElementClasses = createClassList(currentElement.className);
-
-      // console.log(currentElementClasses);
-      categorizedClasses = tailwindClassBuilder(
+      const categorizedClasses = tailwindClassBuilder(
         currentElementClasses,
-        tailwindClassList,
+        tailwindClassList
       );
-      sortedClassList = sortTailwindClasses(categorizedClasses, userOrderPref);
-      sortedClassString = rewriteElement(sortedClassList);
-      currentElement.className = sortedClassString;
-      console.log(sortedClassString);
+
+      const sortedClassList = sortTailwindClasses(
+        categorizedClasses,
+        userOrderPref
+      );
+
+      const sortedClassString = rewriteElement(sortedClassList);
+
+      element.className = sortedClassString;
     }
+  });
 
-    if (currentElement.nextElementSibling) {
-      // replaced: currentElement.nextElement
-      let parent = currentElement.parentElement;
-      while (parent && !currentElement.nextElementSibling) {
-        currentElement = parent.nextElementSibling;
-        parent = parent.parentElement;
-      }
-    }
-
-    currentElement = currentElement.nextElementSibling; // replaced: currentElement.nextElement
-    // console.log(currentElement);
-  }
-
-  let final = "<!doctype html>" + document.documentElement.innerHTML;
-  // console.log(final);
+  return "<!doctype html>\n" + soup.documentElement.outerHTML;
 }
 
-//This function takes the user input file and converts it to a string.
+function downloadFile(content, filename = "updated.html") {
+  const blob = new Blob([content], { type: "text/html" });
+  const url = URL.createObjectURL(blob);
 
-// function htmlString() {
-//   return new Promise((resolve) => {
-//     const userFileInput = document.getElementById("userFileInput");
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = filename;
+  a.click();
 
-//     userFileInput.addEventListener("change", async (event) => {
-//       const htmlFile = event.target.files[0];
-//       const readObj = new FileReader();
-//       readObj.readAsText(htmlFile);
-//       readObj.onload = (e) => {
-//         resolve(e.target.result);
-//       };
-//     });
-//   });
-// }
+  URL.revokeObjectURL(url);
+}
 
 async function main() {
-  // const fileContents = getFileContents();
   const htmlContent = await readData();
-  // console.log(htmlContent);
   const soup = createSoup(htmlContent);
-  // console.log(soup);
-  iterateElements(soup);
+  const finalDocument = iterateElements(soup);
+
+  console.log(finalDocument);
+  downloadFile(finalDocument);
 }
 
 main();
