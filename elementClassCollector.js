36658ddbc01229ff661aad82c6aaf1d7443bcabf
Edward Kim <hkim503@my.bcit.ca>
2026-03-14 18:09:01 -0700

    sam is helping me

--- elementClassCollector.js
+++ elementClassCollector.js
@@ -1,13 +1,40 @@
+import tailwindClassList from "./tailwind-classes.json" with { type: "json"}
+
+console.log(tailwindClassList)
+
+let textToParse = "<div class='mx-py delay-75 bg-fixed'>"
+console.log(textToParse)
+// const flatList = Object.values(tailwindClassList).flat()
+
 function elementClassCollector(textToParse) {
-    
-textToParse = "<div class='mx-py delay-75 bg-fixed'>"
-
-    const new_list = []
-
-    let match = textToParse
-    new_list.push(match)
-
-    return new_list
+    let start = textToParse.split("class='")[1].split(' ').split("'")[0]
 }
 
-console.log(elementClassCollector())
+console.log(elementClassCollector(textToParse))
+
+// let classObjects = {
+//     border: "bg-fixed",
+//     delay: "delay-75",
+//     blockelement: "mx-py"
+// }
+
+// function elementClassCollector(textToParse) {
+
+//     textToParse = "<div class='mx-py delay-75 bg-fixed>"
+
+//     const new_list = []
+
+//     let match = textToParse
+//     new_list.push(match)
+
+//     return new_list
+    
+
+//     const objects = ""
+
+//     let isTailwind = classObjects
+// }
+
+// console.log(elementClassCollector())
+
+// take the list from the function and figure out if there is any Tailwind class or not and return True if there is any Tailwind class
